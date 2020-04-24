a93c3423d607fb4594ee8f70ab75fb7f70baa858
FjollaBerisha <63357710+FjollaBerisha@users.noreply.github.com>
2020-04-24 16:06:59 +0200

    cng

--- app/src/main/java/com/example/remapp/MainActivity.java
+++ app/src/main/java/com/example/remapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.remapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.jar.Attributes;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 private EditText Name;
 private  EditText Password;
 private Button LogIn;
+private TextView Attempts;
+private  int Counter=3;
 
 
     @Override
@@ -25,12 +29,13 @@ private Button LogIn;
         Name=(EditText)findViewById(R.id.etName);
         Password=(EditText)findViewById(R.id.edtPassword);
         LogIn=(Button)findViewById(R.id.btnlogin);
+        Attempts=(TextView)findViewById(R.id.text1);
 
         LogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 validate(Name.getText().toString(),Password.getText().toString());
-                 
+
             }
         });
     }
@@ -41,6 +46,16 @@ private Button LogIn;
                 Intent Inten=new Intent(MainActivity.this,SecondActivity.class);
                 startActivity(Inten);
              }
+            else
+            {
+                Counter--;
+                Attempts.setText("0 Attempts remaining"+String.valueOf(Counter));
+
+                if(Counter==0)
+                {
+                    LogIn.setEnabled(false);
+                }
+            }
 
 
         }
