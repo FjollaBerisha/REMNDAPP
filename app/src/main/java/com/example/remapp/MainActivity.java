e2b2faf9da0035b0ade0fc0defd2a861a8736e8f
FjollaBerisha <63357710+FjollaBerisha@users.noreply.github.com>
2020-04-24 14:15:28 +0200

    Func mainactivity

--- app/src/main/java/com/example/remapp/MainActivity.java
+++ app/src/main/java/com/example/remapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.remapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
+private EditText Name;
+private  EditText Password;
+private Button LogIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Name=(EditText)findViewById(R.id.etName);
+        Password=(EditText)findViewById(R.id.edtPassword);
+        LogIn=(Button)findViewById(R.id.btnlogin);
     }
+        private  void validate(String username,String password)
+        {
+            if( (username=="fjolla")&&(password=="1234"))
+            {
+                Intent Inten=new Intent(MainActivity.this,SecondActivity.class);
+        }
+        }
+
+
+
 }
