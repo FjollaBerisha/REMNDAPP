47f1444f180ecaff44a313c5d9d8dc21bce4f524
FjollaBerisha <63357710+FjollaBerisha@users.noreply.github.com>
2020-04-24 16:42:55 +0200

    lidhjaf

--- app/src/main/java/com/example/remapp/ThirdActivity.java
+++ app/src/main/java/com/example/remapp/ThirdActivity.java
@@ -2,7 +2,10 @@ package com.example.remapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        Button button=(Button)findViewById(R.id.bTNADD);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                OpenFourthActivity();
+
+            }
+        });
     }
+    public  void  OpenFourthActivity()
+    {
+        Intent intent=new Intent(this,FourthActivity.class);
+        startActivity(intent);
+    }
+
 }
